3df55919b9e8732e5874cb740caa17ad48e5db56
Damian Pasterz <damian.pasterz@gmail.com>
2023-03-09 14:28:28 +0100

    fix2

--- src/app/core/services/input.service.ts
+++ src/app/core/services/input.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { machingPiont, VIMEO_ID_CHECK, VIMEO_ONLY_ID_CHECK, YOUTUBE_ID_CHECK, YOUTUBE_ONLY_ID_CHECK } from '@core/models/matching.models';
 import { InputData } from '@core/models/input.model';
 import { VideoServices } from '@core/models/videoServices.model';
@@ -10,22 +11,22 @@ export class InputService {
   public inputData: InputData 
   constructor() {}
 
-public onProviderAndIdCheck(url:string) {
-  if(url.includes(VideoServices.vimeo) || url.match(VIMEO_ID_CHECK) ){
-    this.inputData={provider: VideoServices.vimeo, id: url.match(VIMEO_ID_CHECK)![1]}
-    return console.log(this.inputData)
-  } 
-  if(url.includes(VideoServices.youtube) || url.match(YOUTUBE_ID_CHECK) ){
-    this.inputData={provider: VideoServices.youtube, id: url.match(YOUTUBE_ID_CHECK)![1]}
-    return console.log(this.inputData)
-  } 
-  if(url.length === machingPiont.vimeoIDLength || url.match(VIMEO_ONLY_ID_CHECK) ){
-    this.inputData={provider: VideoServices.vimeo, id: url.match(VIMEO_ONLY_ID_CHECK)![0]}
-    return console.log(this.inputData)
-  } 
-  if(url.length >= machingPiont.youtubeIDLength || url.match(YOUTUBE_ONLY_ID_CHECK) ){
-    this.inputData={provider: VideoServices.youtube, id: url.match(YOUTUBE_ONLY_ID_CHECK)![0]}
-    return console.log(this.inputData)
-  }else return  this.inputData={provider: VideoServices.incorect, id: null}
- }
+  public onProviderAndIdCheck(url:string) {
+    if(url.includes(VideoServices.vimeo) || url.match(VIMEO_ID_CHECK) ){
+      this.inputData={provider: VideoServices.vimeo, id: url.match(VIMEO_ID_CHECK)![1]}
+      return this.inputData
+    } 
+    if(url.includes(VideoServices.youtube) || url.match(YOUTUBE_ID_CHECK) ){
+      this.inputData={provider: VideoServices.youtube, id: url.match(YOUTUBE_ID_CHECK)![1]}
+      return this.inputData
+    } 
+    if(url.length === machingPiont.vimeoIDLength || url.match(VIMEO_ONLY_ID_CHECK) ){
+      this.inputData={provider: VideoServices.vimeo, id: url.match(VIMEO_ONLY_ID_CHECK)![0]}
+      return this.inputData
+    } 
+    if(url.length >= machingPiont.youtubeIDLength || url.match(YOUTUBE_ONLY_ID_CHECK) ){
+      this.inputData={provider: VideoServices.youtube, id: url.match(YOUTUBE_ONLY_ID_CHECK)![0]}
+      return this.inputData
+    }else return  this.inputData={provider: VideoServices.incorect, id: null}
+  }
 }
